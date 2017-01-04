e47a1a3f2234d7712acbffcfb944bdc63a4b2098
Gregory Kikola <gkikola@gmail.com>
2017-01-04 06:51:53 -0500

    Create the window

--- src/application.cpp
+++ src/application.cpp
@@ -4,15 +4,37 @@
 
 #include "application.h"
 
+const int default_win_width = 800;
+const int default_win_height = 600;
+const std::string default_win_title = "Nonny";
+
 Application::Application() {
-  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-    std::string err_msg("SDL_Init: ");
-    err_msg += SDL_GetError();
-    throw std::runtime_error(err_msg);
-  }
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) SDL_error("SDL_Init");
+
+  window = SDL_CreateWindow(default_win_title.c_str(),
+                            SDL_WINDOWPOS_CENTERED,
+                            SDL_WINDOWPOS_CENTERED,
+                            default_win_width, default_win_height,
+                            SDL_WINDOW_RESIZABLE);
+
+  if (!window) SDL_error("SDL_CreateWindow");
 }
 
 Application::~Application() {
+  SDL_cleanup();
+}
+
+void Application::SDL_error(const std::string& function) {
+  std::string err_msg = function;
+  err_msg += ": ";
+  err_msg += SDL_GetError();
+
+  SDL_cleanup();
+  throw std::runtime_error(err_msg);
+}
+
+void Application::SDL_cleanup() {
+  if (window) SDL_DestroyWindow(window);
   SDL_Quit();
 }
 
--- src/application.h
+++ src/application.h
@@ -1,12 +1,20 @@
 #ifndef NONNY_APP_H
 #define NONNY_APP_H
 
+#include <string>
+#include <SDL2/SDL.h>
+
 class Application {
 public:
   Application();
   ~Application();
 
   void run();
+private:
+  void SDL_error(const std::string& function);
+  void SDL_cleanup();
+
+  SDL_Window* window;
 };
 
 #endif
