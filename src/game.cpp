ebce5ce293c1a28247093e84197b886fe695bc47
Gregory Kikola <gkikola@gmail.com>
2017-01-06 00:25:40 -0500

    Add access functions to Game class

--- src/game.cpp
+++ src/game.cpp
@@ -1,6 +1,10 @@
 #include "game.h"
 
-Game::Game() : m_puzzle{nullptr} {
+const int cell_size_step = 8;
+const int cell_age_rate = 50;
+
+Game::Game() : m_puzzle{nullptr}, m_x{0}, m_y{0}, m_cell_size{16} {
+  m_state = GameState::puzzle;
 }
 
 Game::~Game() {
--- src/game.h
+++ src/game.h
@@ -3,16 +3,28 @@
 
 #include "puzzle.h"
 
+enum class GameState { main_menu, opts_menu, puzzle_selection, puzzle };
+
 class Game {
  public:
   Game();
   ~Game();
 
+  GameState game_state() const { return m_state; }
   void update(int elapsed_time);
 
   void load_puzzle(const std::string& filename);
+
+  void get_puzzle_coords(int* x, int* y) const;
+  int cell_size() const { return m_cell_size; }
+
+  const Puzzle& puzzle() const { return *m_puzzle; }
  private:
   Puzzle* m_puzzle;
+  
+  GameState m_state;
+  int m_x, m_y;
+  int m_cell_size;
 };
 
 #endif
