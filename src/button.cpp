c91478f470986d3d8a96b04f7aaf55800dc701bd
Gregory Kikola <gkikola@gmail.com>
2017-01-09 04:41:35 -0500

    Add Button class

---
+++ src/button.cpp
@@ -0,0 +1,30 @@
+#include "game.h"
+
+#include "button.h"
+
+Button::Button(Game* game) : Control{game}, m_hover{false}, m_selected{false} {
+}
+
+void Button::hover_mouse(bool hovering) {
+  m_hover = hovering;
+}
+
+void Button::select() {
+  m_selected = true;
+}
+
+void Button::deselect() {
+  m_selected = false;
+}
+
+void Button::set_label(const std::string& label) {
+  m_label = label;
+}
+
+void Button::register_callback(callback function) {
+  m_callback_func = function;
+}
+
+void Button::activate() {
+  (*m_callback_func)(m_game);
+}
---
+++ src/button.h
@@ -0,0 +1,32 @@
+#ifndef NONNY_BUTTON_H
+#define NONNY_BUTTON_H
+
+#include "control.h"
+
+class Game;
+
+class Button : public Control {
+ public:
+  Button(Game* game);
+
+  void hover_mouse(bool hovering = true);
+  bool is_mouse_hovering() const { return m_hover; }
+
+  void select();
+  void deselect();
+  bool is_selected() const { return m_selected; }
+
+  const std::string& label() const { return m_label; }
+  void set_label(const std::string& label);
+
+  typedef void (*callback)(Game*);
+  void register_callback(callback function);
+  void activate();
+ private:
+  std::string m_label;
+  bool m_hover;
+  bool m_selected;
+  callback m_callback_func;
+};
+
+#endif
