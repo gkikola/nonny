e3b37765bec5f83546b8993220e6e017fdff70a4
Gregory Kikola <gkikola@gmail.com>
2017-04-05 04:13:39 -0400

    Move main.cpp into nonny subdirectory

---
+++ src/nonny/main.cpp
@@ -0,0 +1,38 @@
+/* Nonny -- Play and create nonogram puzzles.
+ * Copyright (C) 2017 Gregory Kikola.
+ *
+ * This file is part of Nonny.
+ *
+ * Nonny is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Nonny is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Nonny.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/* Written by Gregory Kikola <gkikola@gmail.com>. */
+
+#include <iostream>
+#include <stdexcept>
+#include <SDL2/SDL.h>
+
+#include "application.h"
+
+int main(int argc, char* argv[]) {
+  try {
+    Application app;
+    app.run();
+  }
+  catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return 1;
+  }
+  
+  return 0;
+}
