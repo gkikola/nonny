555c71491c87d9776e39feb0207015e262a3a53a
Gregory Kikola <gkikola@gmail.com>
2017-04-07 00:06:04 -0400

    Removed old (pre-rewrite) source files
    They're still available in the oldsrc directory, but no longer used
    in the build

--- src/nonny/main.cpp
+++ src/nonny/main.cpp
@@ -22,13 +22,9 @@
 #include <stdexcept>
 #include <SDL2/SDL.h>
 
-#include "application.h"
-
 int main(int argc, char* argv[])
 {
   try {
-    Application app;
-    app.run();
   }
   catch (const std::exception& e) {
     std::cerr << e.what() << std::endl;
