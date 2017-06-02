880bc636ca89ceaf31f8c1d08f122ce134af6fca
Gregory Kikola <gkikola@gmail.com>
2017-06-02 02:57:02 -0400

    Solver: allow solver to make guesses

--- src/solver/solver.cpp
+++ src/solver/solver.cpp
@@ -21,15 +21,14 @@
 #include "solver/solver.hpp"
 
 #include <algorithm>
+#include <stdexcept>
 #include "solver/line_solver.hpp"
 
 Solver::Solver(Puzzle& puzzle)
-  : m_puzzle(puzzle)
+  : m_puzzle(puzzle),
+    m_row_priority(puzzle.height(), 1),
+    m_col_priority(puzzle.width(), 1)
 {
-  for (int i = 0; i < m_puzzle.width(); ++i)
-    m_cols_to_check.insert(i);
-  for (int j = 0; j < m_puzzle.height(); ++j)
-    m_rows_to_check.insert(j);
 }
 
 bool Solver::step()
@@ -38,58 +37,312 @@ bool Solver::step()
     return true;
   if (m_inconsistent)
     return false;
-
-  while (!m_rows_to_check.empty()) {
-    int row = *m_rows_to_check.begin();
-    PuzzleLine line = m_puzzle.get_row(row);
-    if (!solve_line(line, m_use_complete)) {
-      m_inconsistent = true;
-      return false;
-    }
-  }
-  while (!m_cols_to_check.empty()) {
-    int col = *m_cols_to_check.begin();
-    PuzzleLine line = m_puzzle.get_col(col);
-    if (!solve_line(line, m_use_complete)) {
-      m_inconsistent = true;
-      return false;
-    }
-  }
-
-  //switch to complete line solver if the fast one didn't produce any info
-  if (!m_use_complete
-      && m_rows_to_check.empty() && m_cols_to_check.empty()) {
-    m_use_complete = true;
-    for (int i = 0; i < m_puzzle.width(); ++i) {
-      if (m_cols_solved.count(i) == 0)
-        m_cols_to_check.insert(i);
-    }
-    for (int j = 0; j < m_puzzle.height(); ++j) {
-      if (m_rows_solved.count(j) == 0)
-        m_rows_to_check.insert(j);
-    }
-  }
   
-  return is_finished();
-}
-
-void Solver::operator()()
-{
+  const int num_iters = 16;
+  
   int count = 0;
-  while (count < 32) {
-    for (int row = 0; row < m_puzzle.height(); ++row) {
-      PuzzleLine line = m_puzzle.get_row(row);
-      LineSolver solver(line);
-      solver();
+  while (count < num_iters && is_line_available()) {
+    LineType type;
+    int index = -1;
+
+    //alternate between rows and columns
+    if (count % 2 == 0) {
+      int row = select_row();
+      if (row >= 0) {
+        type = LineType::row;
+        index = row;
+      }
+    } else {
+      int col = select_col();
+      if (col >= 0) {
+        type = LineType::column;
+        index = col;
+      }
     }
-    for (int col = 0; col < m_puzzle.width(); ++col) {
-      PuzzleLine line = m_puzzle.get_col(col);
-      LineSolver solver(line);
-      solver();
+
+    if (index >= 0) { //found a line, send it to line solver
+      PuzzleLine line(m_puzzle, index, type);
+      if (!solve_line(line, m_use_complete)) {
+        //found a contradiction
+        backtrack();
+        return false;
+      }
     }
 
     ++count;
   }
+
+  int num_rows_solved = m_rows_solved.size();
+  int num_cols_solved = m_cols_solved.size();
+  if (num_rows_solved == m_puzzle.height()
+      && num_cols_solved == m_puzzle.width()) {
+    record_solution();
+    
+    if (m_alternatives.empty()) {
+      //no other possibilities, so we're done
+      m_finished = true;
+    } else {
+      //backtrack to find alternative solutions
+      backtrack();
+    }
+  }
+
+  if (!m_finished) {
+    //if the complete solver found new information,
+    //switch back to the fast solver
+    bool line_available = is_line_available();
+    if (m_use_complete && !line_available) {
+      //complete solver found nothing, need to make a guess
+      guess();
+      m_use_complete = false;
+    }
+
+    //if complete solver found new information, switch back to fast
+    if (m_new_info_found && m_use_complete) {
+      m_new_info_found = false;
+      m_use_complete = false;
+    }
+
+    //switch to complete line solver if the fast one didn't produce any info
+    if (!m_use_complete && !line_available) {
+      m_use_complete = true;
+      for (int i = 0; i < m_puzzle.width(); ++i) {
+        if (m_cols_solved.find(i) == m_cols_solved.end())
+          m_col_priority[i] = 1;
+        else
+          m_col_priority[i] = 0;
+      }
+      for (int j = 0; j < m_puzzle.height(); ++j) {
+        if (m_rows_solved.find(j) == m_rows_solved.end())
+          m_row_priority[j] = 1;
+        else
+          m_row_priority[j] = 0;
+      }
+    }
+  }
+  
+  return m_finished;
+}
+
+void Solver::operator()()
+{
+  while (!step()) { }
+}
+
+int Solver::select_row()
+{
+  int max_row = -1;
+  int row_priority = 0;
+  for (unsigned j = 0; j < m_row_priority.size(); ++j) {
+    if (m_row_priority[j] > row_priority) {
+      max_row = j;
+      row_priority = m_row_priority[j];
+    }
+  }
+
+  if (max_row < 0) {
+    return -1;
+  } else {
+    m_last_row_selected = max_row;
+    m_row_priority[max_row] = 0;
+    return max_row;
+  }
+}
+
+int Solver::select_col()
+{
+  int max_col = -1;
+  int col_priority = 0;
+  for (unsigned i = 0; i < m_col_priority.size(); ++i) {
+    if (m_col_priority[i] > col_priority) {
+      max_col = i;
+      col_priority = m_col_priority[i];
+    }
+  }
+
+  if (max_col < 0) {
+    return -1;
+  } else {
+    m_last_col_selected = max_col;
+    m_col_priority[max_col] = 0;
+    return max_col;
+  }
+}
+
+bool Solver::is_line_available()
+{
+  for (int i : m_col_priority) {
+    if (i > 0)
+      return true;
+  }
+  for (int j : m_row_priority) {
+    if (j > 0)
+      return true;
+  }
+  return false;
+}
+
+void Solver::check_for_solved_lines()
+{
+  m_rows_solved.clear();
+  m_cols_solved.clear();
+  for (int i = 0; i < m_puzzle.width(); ++i) {
+    PuzzleLine line = m_puzzle.get_col(i);
+    if (line.is_solved())
+      m_cols_solved.insert(i);
+  }
+  for (int j = 0; j < m_puzzle.height(); ++j) {
+    PuzzleLine line = m_puzzle.get_row(j);
+    if (line.is_solved())
+      m_rows_solved.insert(j);
+  }
+}
+
+void Solver::backtrack()
+{
+  if (m_alternatives.empty()) {
+    //no alternatives
+    //if no solutions were found yet, then the puzzle is inconsistent
+    if (num_solutions() == 0)
+      m_inconsistent = true;
+
+    m_finished = true;
+  } else {
+    //backtrack to last alternative
+    m_puzzle.load_state(m_alternatives.top().puzzle_state);
+
+    //reset line priorities
+    for (int& i : m_col_priority)
+      i = 0;
+    for (int& j : m_row_priority)
+      j = 0;
+    m_col_priority[m_alternatives.top().col] = 1;
+    m_row_priority[m_alternatives.top().row] = 1;
+
+    //regenerate solved list
+    check_for_solved_lines();
+
+    m_alternatives.pop();
+  }
+}
+
+void Solver::guess()
+{
+  const int max_search_dist = 4;
+
+  //increment guess counter
+  ++m_num_guesses;
+
+  //try to locate a good cell for a guess
+  int best_x = -1, best_y = -1;
+  int max_pot = 0;
+
+  //calculate potential of nearby cells
+  for (int y = m_last_row_selected - max_search_dist;
+       y < m_last_row_selected + max_search_dist;
+       ++y) {
+    for (int x = m_last_col_selected - max_search_dist;
+         x < m_last_col_selected + max_search_dist;
+         ++x) {
+      int potential = cell_potential(x, y);
+      if (potential > max_pot) {
+        best_x = x;
+        best_y = y;
+        max_pot = potential;
+      }
+    }
+  }
+
+  //if we couldn't find any candidates nearby, just search
+  //the whole grid and pick the first undetermined cell
+  if (max_pot == 0) {
+    for (int y = 0; y < m_puzzle.height(); ++y) {
+      for (int x = 0; x < m_puzzle.width(); ++x) {
+        if (m_puzzle[x][y].state == PuzzleCell::State::blank) {
+          best_x = x;
+          best_y = y;
+          y = m_puzzle.height(); //break outer loop
+          break;
+        }
+      }
+    }
+  }
+
+  //if there are no blank cells, why are we here?
+  if (best_x < 0 || best_y < 0)
+    throw std::runtime_error("Solver::guess: "
+                             "guess called on finished puzzle");
+  
+  //we have our guess, now make the branches
+  SolverState state;
+  state.row = best_y;
+  state.col = best_x;
+  CompressedState grid_state;
+
+  //cross out cell and push state onto the stack
+  m_puzzle.cross_out_cell(best_x, best_y);
+  m_puzzle.copy_state(grid_state);
+  state.puzzle_state = std::move(grid_state);
+  m_alternatives.push(state);
+
+  //try each different color and push states onto the stack
+  auto first = m_puzzle.palette().begin();
+  if (first->name == "background") ++first;
+  auto it = first;
+  ++it;
+  while (it != m_puzzle.palette().end()
+         && it->name != "background") {
+    grid_state = CompressedState();
+    m_puzzle.mark_cell(best_x, best_y, it->color);
+    m_puzzle.copy_state(grid_state);
+    state.puzzle_state = std::move(grid_state);
+    m_alternatives.push(state);
+  }
+
+  //try first color and continue on from there
+  m_puzzle.mark_cell(best_x, best_y, first->color);
+  m_col_priority[best_x] = 2;
+  m_row_priority[best_y] = 2;
+}
+
+int Solver::cell_potential(int x, int y)
+{
+  if (x < 0 || x >= m_puzzle.width()
+      || y < 0 || y >= m_puzzle.height())
+    return 0;
+
+  if (m_puzzle[x][y].state != PuzzleCell::State::blank)
+    return 0;
+  
+  return num_filled_neighbors(x, y);
+}
+
+int Solver::num_filled_neighbors(int x, int y)
+{
+  int count = 0;
+  if (x > 0 && m_puzzle[x - 1][y].state != PuzzleCell::State::blank)
+    ++count;
+  else if (x == 0) //edge cells are useful so count them too
+    ++count;
+
+  if (y > 0 && m_puzzle[x][y - 1].state != PuzzleCell::State::blank)
+    ++count;
+  else if (y == 0)
+    ++count;
+
+  if (x + 1 < m_puzzle.width()
+      && m_puzzle[x + 1][y].state != PuzzleCell::State::blank)
+    ++count;
+  else if (x + 1 == m_puzzle.width())
+    ++count;
+  
+  if (y + 1 < m_puzzle.height()
+      && m_puzzle[x][y + 1].state != PuzzleCell::State::blank)
+    ++count;
+  else if (y + 1 == m_puzzle.height())
+    ++count;
+
+  return count;
 }
 
 bool Solver::solve_line(PuzzleLine& line, bool complete)
@@ -110,14 +363,16 @@ bool Solver::solve_line(PuzzleLine& line, bool complete)
     if (m_solved_line[i].state == PuzzleCell::State::blank)
       is_solved = false;
     
-    //line solver produced new information, update puzzle line
-    //and record changed perpendicular lines
+    //if line solver produced new information, update puzzle line
+    //and boost priority of changed perpendicular lines
     if (line[i] != m_solved_line[i]
         && m_solved_line[i].state != PuzzleCell::State::blank) {
+      m_new_info_found = true;
+
       if (line.type() == LineType::row)
-        m_cols_to_check.insert(i);
+        ++m_col_priority[i];
       else
-        m_rows_to_check.insert(i);
+        ++m_row_priority[i];
 
       switch (m_solved_line[i].state) {
       case PuzzleCell::State::filled:
@@ -132,15 +387,27 @@ bool Solver::solve_line(PuzzleLine& line, bool complete)
     }
   }
 
-  if (line.type() == LineType::row) {
-    if (is_solved)
+  if (is_solved) {
+    if (line.type() == LineType::row)
       m_rows_solved.insert(line.index());
-    m_rows_to_check.erase(line.index());
-  } else {
-    if (is_solved)
+    else
       m_cols_solved.insert(line.index());
-    m_cols_to_check.erase(line.index());
   }
   
   return true;
+}
+
+void Solver::record_solution()
+{
+  CompressedState sol;
+  m_puzzle.copy_state(sol);
+
+  //check to see if this solution was already found
+  for (auto& e : m_solutions) {
+    if (e == sol)
+      return;
+  }
+  
+  //otherwise, record the solution
+  m_solutions.push_back(std::move(sol));
 }
--- src/solver/solver.hpp
+++ src/solver/solver.hpp
@@ -24,8 +24,18 @@
 #include <set>
 #include <stack>
 #include <vector>
+#include "puzzle/compressed_state.hpp"
 #include "puzzle/puzzle.hpp"
 #include "puzzle/puzzle_cell.hpp"
+
+/*
+ * The state of the solver at a branch point.
+ */
+struct SolverState {
+  CompressedState puzzle_state;
+  int row = 0; //where the guess was made
+  int col = 0;
+};
 
 /*
  * Takes a reference to a puzzle and, when invoked, solves that
@@ -35,46 +45,88 @@ class Solver {
 public:
   Solver(Puzzle& puzzle);
 
-  // Execute one step in solving the puzzle, returns true if solved
+  // Execute one step in solving the puzzle, returns true if finished
   bool step();
   
-  // Solve the puzzle
+  // Solve the whole puzzle, all at once
   void operator()();
 
-  // Is the puzzle solved?
-  inline bool is_finished() const;
+  /*
+   * Has the solver finished running? Even if the puzzle has been
+   * solved the solver may continue to run in order to find additional
+   * solutions
+   */
+  bool is_finished() const { return m_finished; }
 
   // How many solutions were found?
-  int num_solutions() const { if (is_finished()) return 1; else return 0; }
+  int num_solutions() const { return m_solutions.size(); }
 
+  // How many guesses did the solver have to make?
+  int num_guesses() const { return m_num_guesses; }
+  
   // Can the puzzle be solved one line at a time?
-  bool is_line_solvable() const { return !m_used_guessing; }
+  inline bool is_line_solvable() const;
 
   // Is there an inconsistency in the puzzle?
   bool was_contradiction_found() const { return m_inconsistent; }
   
 private:
+  // Choose a line to solve
+  int select_row();
+  int select_col();
+  bool is_line_available();
+
+  // Update solved line information
+  void check_for_solved_lines();
+  
+  // Return to the most recent alternative guess state
+  void backtrack();
+
+  // Create a branch point and make a guess
+  void guess();
+
+  // How good a cell is for guessing, higher is better,
+  // 0 means the cell is already determined
+  int cell_potential(int x, int y);
+  
+  int num_filled_neighbors(int x, int y);
+  
   // Returns false on contradiction
   bool solve_line(PuzzleLine& line, bool complete = false);
+
+  // Store the solution in the solution list
+  void record_solution();
+  
   Puzzle& m_puzzle;
   std::vector<PuzzleCell> m_solved_line;
 
-  std::set<int> m_rows_to_check;
-  std::set<int> m_cols_to_check;
+  // Solutions found and alternatives to consider
+  std::vector<CompressedState> m_solutions;
+  std::stack<SolverState> m_alternatives;
+
+  // Priority and solved lines
+  std::vector<int> m_row_priority;
+  std::vector<int> m_col_priority;
   std::set<int> m_rows_solved;
   std::set<int> m_cols_solved;
-  bool m_used_guessing = false;
-  bool m_inconsistent = false;
-  bool m_use_complete = false;
+
+  // Used to guide the guesser
+  int m_last_row_selected = 0;
+  int m_last_col_selected = 0;
+
+  bool m_finished = false; //are we done?
+  int m_num_guesses = 0; //how many guesses have we made?
+  bool m_inconsistent = false; //is puzzle contradictory?
+  bool m_use_complete = false; //use the complete rather than fast linesolver
+  bool m_new_info_found = false; //did the line solver find new info?
 };
 
 
 /* implementation */
 
-inline bool Solver::is_finished() const
+bool Solver::is_line_solvable() const
 {
-  return static_cast<int>(m_rows_solved.size()) == m_puzzle.height()
-    && static_cast<int>(m_cols_solved.size()) == m_puzzle.width();
+  return is_finished() && m_num_guesses == 0 && m_solutions.size() > 0;
 }
 
 #endif
